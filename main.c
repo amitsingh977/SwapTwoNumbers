6e2a909711d44d2c9b356c1fd3cded3db7cc58e9
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-21 14:41:56 +1100

    main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  SwapTwoNumbers
+//
+//  Created by Eun Jae Lee on 21/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+//  Temporary variable
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    int a,b,temp;
+    
+    printf("Enter Two Numbers \n");
+    scanf("%d%d", &a,&b);
+    
+    temp = a;
+    a = b;
+    b = temp;
+    
+    printf("Your first number is now %d\n",temp);
+    
+    return 0;
+}
